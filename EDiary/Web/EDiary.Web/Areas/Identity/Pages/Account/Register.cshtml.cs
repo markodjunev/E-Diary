791c8605b29f387a8037f42b2ce0882464294597
Marchelloo <49792565+markodjunev@users.noreply.github.com>
2020-11-09 14:21:10 +0200

    Disable registration

---
+++ EDiary/Web/EDiary.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,20 @@
+﻿namespace EDiary.Web.Areas.Identity.Pages.Account
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        public IActionResult OnGetAsync(string returnUrl = null)
+        {
+            return this.RedirectToPage("Login");
+        }
+
+        public IActionResult OnPost()
+        {
+            return this.RedirectToPage("Login");
+        }
+    }
+}
