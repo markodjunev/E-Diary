9244754986ed7df9328634cb465a4126a8d4638d
Marchelloo <49792565+markodjunev@users.noreply.github.com>
2020-12-31 12:27:42 +0200

    Update ScheduleSubjectsClassesController.cs

--- EDiary/Web/EDiary.Web/Controllers/ScheduleSubjectsClassesController.cs
+++ EDiary/Web/EDiary.Web/Controllers/ScheduleSubjectsClassesController.cs
@@ -1,36 +1,46 @@
 ﻿namespace EDiary.Web.Controllers
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Threading.Tasks;
 
+    using EDiary.Common;
+    using EDiary.Data.Models;
     using EDiary.Services.Data.Interfaces;
     using EDiary.Web.ViewModels.Common.ScheduleSubjectsClasses.OutputViewModels;
     using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
 
     public class ScheduleSubjectsClassesController : BaseController
     {
         private readonly IScheduleSubjectsClassesService scheduleSubjectsClassesService;
         private readonly ISubjectsClassesService subjectsClassesService;
+        private readonly UserManager<ApplicationUser> userManager;
 
         public ScheduleSubjectsClassesController(
             IScheduleSubjectsClassesService scheduleSubjectsClassesService,
-            ISubjectsClassesService subjectsClassesService)
+            ISubjectsClassesService subjectsClassesService,
+            UserManager<ApplicationUser> userManager)
         {
             this.scheduleSubjectsClassesService = scheduleSubjectsClassesService;
             this.subjectsClassesService = subjectsClassesService;
+            this.userManager = userManager;
         }
 
         [Authorize(Roles = "Administrator,Principal")]
-        public IActionResult Schedule(int id)
+        public async Task<IActionResult> Schedule(int id)
         {
             var subjectClass = this.subjectsClassesService.GetById(id);
 
             if (subjectClass == null)
             {
                 return this.RedirectToAction("Error", "Home", new { area = string.Empty });
+            }
+
+            var user = await this.userManager.GetUserAsync(this.User);
+            var isPrincipal = await this.userManager.IsInRoleAsync(user, GlobalConstants.PrincipalRoleName);
+            if (isPrincipal == true && user.SchoolId != subjectClass.SchoolId)
+            {
+                return this.RedirectToAction("Error", "Home", new { area = string.Empty, });
             }
 
             var viewModel = new ScheduleSubjectsClassesViewModel
